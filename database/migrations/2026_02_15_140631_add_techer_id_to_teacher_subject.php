9fcae840a8cabaabbdb257296d2089f1af148085
Ce5arCadena <cesarojitos555@hotmail.com>
2026-02-15 09:14:04 -0500

    Modificación tabla teacher_subject agregando curso

---
+++ database/migrations/2026_02_15_140631_add_techer_id_to_teacher_subject.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teacher_subject', function (Blueprint $table) {
+            $table->unsignedBigInteger('grade_id')->nullable()->after('subject_id');
+            $table->foreign('grade_id')->references('id')->on('grades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teacher_subject', function (Blueprint $table) {
+            $table->dropForeign(['grade_id']);
+            $table->dropColumn('grade_id');
+        });
+    }
+};
