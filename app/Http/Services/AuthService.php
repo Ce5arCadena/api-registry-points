f2e8326aa43cd1b24d1715994ebc09168bc8442d
Ce5arCadena <cesarojitos555@hotmail.com>
2026-02-22 09:01:09 -0500

    CCambiando rol en authservice

--- app/Http/Services/AuthService.php
+++ app/Http/Services/AuthService.php
@@ -39,7 +39,7 @@ class AuthService {
             'message' => 'Login éxitoso',
             'data' => [
                 'token' => $token,
-                'rol' => "TEACHER"
+                'rol' => $user->role
             ]
         ]);
     }
